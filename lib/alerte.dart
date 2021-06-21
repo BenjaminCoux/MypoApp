caa7fded8ccfe717c91106bccf0af19f23ec961a
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-06-21 17:02:18 +0100

    ajout d'affichages des jours pour une alerte

--- lib/alerte.dart
+++ lib/alerte.dart
@@ -21,18 +21,142 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
   }
 }
 
-class AlertScreen extends StatelessWidget{
+class AlertScreen extends StatefulWidget {
   Alert alerte;
+
   AlertScreen({required this.alerte});
+
+  @override
+  _AlertScreenState createState() => _AlertScreenState();
+}
+
+class _AlertScreenState extends State<AlertScreen>{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopBar(),
       body: Center(child: Column(children: [
-        Text(alerte.title),
-        Text(alerte.content)
+        Padding(padding: EdgeInsets.all(10), child: 
+          Row(
+            children: [Text("Titre de l'alerte")],
+          )),
+        Padding(padding: EdgeInsets.all(10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(18),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: d_lightgray,
+                spreadRadius: 4,
+                blurRadius: 6,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),child: Padding(padding: EdgeInsets.all(10),
+        child: Text(widget.alerte.title),),
+        ),),
+        Padding( padding : EdgeInsets.all(10),child:Row(children : [Text("Contenu de l'alerte : ")])),
+        Padding(padding: EdgeInsets.all(10),child:Container( decoration: BoxDecoration(
+      color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(18),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: d_lightgray,
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(widget.alerte.content),
+          ),
+        ),),
+        Container(decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(18),
+          ),boxShadow: [
+          BoxShadow(
+            color: d_lightgray,
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          ),
+        ],),margin: EdgeInsets.fromLTRB(10, 10, 10, 10),child:Column(children: [
+          Container(child:Padding(padding:EdgeInsets.all(12),child:Row(children:[Text("Jours",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22,color: d_green,))]))),
+          Container(
+            child:Padding(padding: EdgeInsets.all(8) ,child:Row(children: [
+              Checkbox(activeColor: d_green,value: widget.alerte.days[0], onChanged:(bool? value)=>{
+                  setState((){
+                    widget.alerte.days[0]=value!;
+                  })
+
+              }),
+              Text("lundi"),
+              Checkbox(activeColor: d_green,value: widget.alerte.days[1], onChanged:(bool? value)=>{
+
+                setState((){
+                  widget.alerte.days[1]=value!;
+                })
+
+              }),
+              Text("Mardi"),
+              Checkbox(activeColor: d_green ,value: widget.alerte.days[2], onChanged:(bool? value)=>{
+
+              setState((){
+                widget.alerte.days[2]=value!;
+              })
+
+              }),
+              Text("Mercredi"),
+              Checkbox(activeColor: d_green,value: widget.alerte.days[3], onChanged:(bool? value)=>{
+
+              setState((){
+                widget.alerte.days[3]=value!;
+              })
+
+              }),
+              Text("Jeudi"),
+            ],),),),
+          Container(
+            child:Padding(padding: EdgeInsets.all(11),child:Row(
+              children: [
+                Checkbox(activeColor: d_green,value: widget.alerte.days[4], onChanged:(bool? value)=>{
+
+                setState((){
+                  widget.alerte.days[4]=value!;
+                })
+
+                }),
+                Text("Vendredi"),
+                Checkbox(activeColor: d_green,value: widget.alerte.days[5], onChanged:(bool? value)=>{
+
+                  setState((){
+                    widget.alerte.days[5]=value!;
+                  })
+
+                }),
+                Text("Samedi"),
+                Checkbox(activeColor: d_green,value: widget.alerte.days[6], onChanged:(bool? value)=>{
+
+                setState((){
+                  widget.alerte.days[6]=value!;
+                })
+
+                }),
+                Text("Dimanche"),
+              ],
+            ),),),])),
       ],),),
+
     );
   }
 }

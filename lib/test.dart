92494e00d60ef0d64c9a60e259cff6dba62683e8
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-06-30 11:40:15 +0100

    fix bug import

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';

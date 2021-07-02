9b0f2d9618d3c5fcc48eed91ee03dde3a894f9a9
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-07-02 15:46:54 +0100

    progression formulaire gestion de clés miltiples

--- lib/pages/formulaire_alerte_auto_page.dart
+++ lib/pages/formulaire_alerte_auto_page.dart
@@ -22,18 +22,21 @@ class FormScreen extends StatefulWidget {
 class _FormState extends State<FormScreen> {
   final alertName = TextEditingController();
   final alertContent = TextEditingController();
+  final keyName = TextEditingController();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   final week = [false, false, false, false, false, false, false];
   final cibles = [false, false, false];
-  int _value=1;
-  int _value2=1;
+  int _value = 1;
+  int _value2 = 1;
   var db;
-
+  List<String> keys = <String>[];
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
     alertContent.dispose();
     alertName.dispose();
+    keyName.dispose();
+    keys.clear();
     super.dispose();
   }
 
@@ -53,65 +56,107 @@ class _FormState extends State<FormScreen> {
     pref.setInt("nombreAlerte", widget.nb + 1);
   }
 
-
-  Widget alertKeys(){
+  Widget alertKeys(BuildContext context) {
     return Container(
-      child:Column(children:[
-        Row(mainAxisAlignment:MainAxisAlignment.center,children: [
-          Container(margin:EdgeInsets.fromLTRB(0,0, 50, 0),child:DropdownButton(
-              value: _value,
-              items: [
-                DropdownMenuItem(
-                  child: Text("Contient"),
-                  value: 1,
-                ),
-                DropdownMenuItem(
-                  child: Text("Ne Contient pas"),
-                  value: 2,
-                ),
-                DropdownMenuItem(
-                  child : Text("Est au debut"),
-                  value:3,
-                ),
-                DropdownMenuItem(
-                  child:Text("Est à la fin"),
-                  value:4,
-                )
-
-              ],
-
-              onChanged: (int? value) {
-                setState(() {
-                  _value = value!;
-                });
-              },
-              hint:Text("Select item")
-          ),),
-          Container(child:DropdownButton(
-              value: _value2,
-              items: [
-                DropdownMenuItem(
-                  child: Text("Accepte"),
-                  value: 1,
-                ),
-                DropdownMenuItem(
-                  child: Text("N'accepte pas"),
-                  value: 2,
-                )
-              ],
-
-              onChanged: (int? value) {
-                setState(() {
-                  _value2 = value!;
-                });
-              },
-              hint:Text("Select item")
-          ),),
-        ],),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+              child: DropdownButton(
+                  value: _value,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("Contient"),
+                      value: 1,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Ne Contient pas"),
+                      value: 2,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Est au debut"),
+                      value: 3,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Est à la fin"),
+                      value: 4,
+                    )
+                  ],
+                  onChanged: (int? value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  },
+                  hint: Text("Select item")),
+            ),
+            Container(
+              child: DropdownButton(
+                  value: _value2,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("Accepte"),
+                      value: 1,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("N'accepte pas"),
+                      value: 2,
+                    )
+                  ],
+                  onChanged: (int? value) {
+                    setState(() {
+                      _value2 = value!;
+                    });
+                  },
+                  hint: Text("Select item")),
+            ),
+          ],
+        ),
+        keys.length > 0
+            ? ListView.builder(
+                shrinkWrap: true,
+                itemCount: keys.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                            onTap: () => {
+                                  setState(() {
+                                    this.keys.remove(keys[index]);
+                                  })
+                                },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(18),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: d_lightgray,
+                                    spreadRadius: 4,
+                                    blurRadius: 6,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child: Text(keys[index]),
+                            ))
+                      ]);
+                })
+            : Text("Pas encore de clés pour cette alerte"),
         Padding(
           padding: EdgeInsets.all(12),
           child: TextField(
-            controller: alertName,
+            onSubmitted: (String? txt) => {
+              setState(() {
+                keys.add(keyName.text);
+                keyName.text = "";
+              })
+            },
+            controller: keyName,
             decoration: InputDecoration(
               labelStyle: TextStyle(color: Colors.black),
               focusedBorder: OutlineInputBorder(
@@ -233,21 +278,22 @@ class _FormState extends State<FormScreen> {
                         ),
                       )),
                 ),
-                Container(decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(18),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: d_lightgray,
-                      spreadRadius: 4,
-                      blurRadius: 6,
-                      offset: Offset(0, 3),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: d_lightgray,
+                          spreadRadius: 4,
+                          blurRadius: 6,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: alertKeys()),
+                    child: alertKeys(context)),
                 Container(
                     decoration: BoxDecoration(
                       color: Colors.white,

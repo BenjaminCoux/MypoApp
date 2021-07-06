42348ef0eab347252c9e82942b7d1dc4d3ac9da5
iago <iagodesouza1999@gmail.com>
2021-07-06 11:49:52 +0200

    day selector widget

---
+++ lib/widget/dayselector_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/date_symbols.dart';
+import 'package:mypo/main.dart';
+import 'package:weekday_selector/weekday_selector.dart';
+
+class daySelectorWidget extends StatefulWidget {
+  @override
+  _daySelectorWidgetState createState() => _daySelectorWidgetState();
+}
+
+class _daySelectorWidgetState extends State<daySelectorWidget> {
+  final DateSymbols fr = dateTimeSymbolMap()['fr'];
+  final values = List.filled(7, false);
+
+  @override
+  Widget build(BuildContext context) {
+    return WeekdaySelector(
+      weekdays: fr.STANDALONEWEEKDAYS,
+      // shortWeekdays: fr.STANDALONENARROWWEEKDAYS,
+      shortWeekdays: fr.STANDALONESHORTWEEKDAYS,
+      firstDayOfWeek: fr.FIRSTDAYOFWEEK + 1,
+      selectedFillColor: d_green,
+      onChanged: (v) {
+        setState(() {
+          values[v % 7] = !values[v % 7];
+        });
+      },
+      values: values,
+    );
+  }
+}

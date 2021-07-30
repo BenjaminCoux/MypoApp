43a463fd7e9699b0428e61a6a54293e058a8df91
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-07-30 14:50:25 +0100

    update grp contact

--- lib/pages/group_contact._page.dart
+++ lib/pages/group_contact._page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mypo/pages/home_page.dart';
 import 'package:mypo/widget/appbar_widget.dart';
 import 'package:mypo/widget/hamburgermenu_widget.dart';
+import 'package:mypo/model/colors.dart';
 
 class GroupContact extends StatefulWidget {
   @override
@@ -12,11 +14,29 @@ class _GroupContactState extends State<GroupContact> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
-      appBar: TopBar(title: "Groupes de contact"),
+      appBar: TopBarRedirection(
+        title: "Groupes de contact",
+        page: () => HomePage(),
+      ),
       body: Center(
-        child: Column(
-          children: [Text("Test")],
-        ),
+        child: Column(children: [
+          OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  backgroundColor: d_darkgray,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              onPressed: () => {},
+              child: Text(
+                "+ Ajouter un groupe de contact",
+                style: TextStyle(
+                    backgroundColor: d_darkgray,
+                    fontSize: 16,
+                    letterSpacing: 2.2,
+                    color: Colors.white,
+                    fontFamily: 'calibri'),
+              )),
+        ]),
       ),
     );
   }
